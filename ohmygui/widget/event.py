a1c5291f2ed0d86b077caace09d0c0e377a55520
Fishgame Studio <popxh@outlook.com>
2026-06-01 17:21:17 +0800

    ⚡️feat interface get_func to get current function

--- ohmygui/widget/event.py
+++ ohmygui/widget/event.py
@@ -7,5 +7,8 @@ class Event:
         self.func = func
     def set_func(self, func: Callable):
         self.func = func
+    @property
+    def get_func(self) -> Callable:
+        return self.func
     def __call__(self, *args, **kwargs):
         return self.func(*args, **kwargs)
