7a94f7b297f10f964836aec2fa9f413f6247a0d6
Fishgame Studio <popxh@outlook.com>
2026-06-14 11:16:50 +0800

    👽Update example/demo

--- example/demo/guessnum.py
+++ example/demo/guessnum.py
@@ -1,5 +1,5 @@
 # Guess number demo.
-# Only 31 lines.
+# Only 34 lines.
 
 from ohmygui import *
 from random import randint
@@ -8,32 +8,34 @@ win = core.Window("Guess Number", (1000, 700))
 win.set_bg("#a0efa0")
 
 target = randint(1, 100)
+cnt = 0
 
 with layout.VerticalLayout() as lay:
     lay.set_common_margin(100).set_common_spacing(30)  
     msg = widget.Text("Guess the number between 1 and 100", "#000000", "#00000000")
 
-    msg.set_font("Consolas 14")  
+    msg.set_font("Consolas 14", 50)  
 
     entry = widget.InputEntry("Enter a valid number...")
     
 
     def submit() -> None:
-        global msg, entry
+        global msg, entry, cnt
+        cnt += 1
         text = entry.value.strip()
         digit: int
+        entry.clear_value()
         try:
             digit = int(text)
         except ValueError:
             msg.set_text("Invalid number!")
-            entry.set_value("")
             return
         if digit > target:
             msg.set_text("Too large!")
         elif digit < target:
             msg.set_text("Too small!")
         else:
-            msg.set_text("Congratulations! You got it!")
+            msg.set_text(f"Congratulations! You got it! \n You guess it for {cnt} times.")
             entry.hide()
     entry.on_submit(submit)
 
