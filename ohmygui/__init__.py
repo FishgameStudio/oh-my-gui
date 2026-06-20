e1c605c978681341a5e8e62b5f93aad49807c5c6
Fishgame Studio <popxh@outlook.com>
2026-06-20 18:51:10 +0800

    ✅Update version & changelog

--- ohmygui/__init__.py
+++ ohmygui/__init__.py
@@ -30,7 +30,7 @@ _logging.basicConfig(
 )
 
 __author__  = "Fishgame Studio"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 from . import core
 from . import widget
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ohmygui",
-    version="1.0.0",
+    version="1.1.0",
     author="FishgameStudio",
     description="A lightweight GUI library wrapping PySide6 for Python",
     long_description=open("README.md", "r", encoding="utf-8").read(),
