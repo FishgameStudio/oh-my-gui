bfbe5c84dce414fa80b72c10aeddd1c7d51d52d4
Fishgame Studio <popxh@outlook.com>
2026-06-21 18:05:32 +0800

    🐛fix typo: horizental -> horizontal

--- ohmygui/layout/__init__.py
+++ ohmygui/layout/__init__.py
@@ -1,10 +1,10 @@
 from .base import *
 from .vertical import *
-from .horizental import *
+from .horizontal import *
 from .grid import *
 from .form import *
 
-__all__ = ["base", "vertical", "horizental", "grid", "form"]
+__all__ = ["base", "vertical", "horizontal", "grid", "form"]
 from logging import info, warning, error, critical
 
 info(f"Module {__name__} loaded")
