437bee1ac83a1278a05d9942028266961dff1b44
Fishgame Studio <popxh@outlook.com>
2026-06-21 14:23:15 +0800

    🤯Fully OMS (Oh My Stylesheet) Support

--- ohmygui/core/__init__.py
+++ ohmygui/core/__init__.py
@@ -2,6 +2,7 @@ from .application import *
 from .window import *
 from .mouse import *
 from .bind import *
+import oms as _OmsParser
 
 __all__ = ['application', 'window', 'mouse', 'bind']
 from logging import info, warning, error, critical
