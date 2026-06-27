a73867c5a37af4ec5d1dbb1cd617756d8d447710
Fishgame Studio <popxh@outlook.com>
2026-06-27 09:32:44 +0800

    ✅add log

--- ohmygui/core/bind.py
+++ ohmygui/core/bind.py
@@ -1,6 +1,7 @@
 # Decorator for binding events.
 
 from typing import Callable as _Callable, Any as _Any
+from logging import info
 
 def bind(register_method: _Callable[[_Callable[..., _Any]], _Any]):
     """
@@ -15,6 +16,7 @@ def bind(register_method: _Callable[[_Callable[..., _Any]], _Any]):
     """
     def decorator(target_func: _Callable[..., _Any]) -> _Callable[..., _Any]:
         register_method(target_func)
+        info(f"bind callback function {target_func} to register method {register_method}")
         return target_func
     return decorator
 
