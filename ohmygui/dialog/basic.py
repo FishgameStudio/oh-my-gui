9ae3ff6ceaa8c063172e2d06c49878e5f60742c6
Fishgame Studio <popxh@outlook.com>
2026-07-05 10:21:59 +0800

    🐛Fix wrong importing symbols

--- ohmygui/dialog/basic.py
+++ ohmygui/dialog/basic.py
@@ -5,14 +5,14 @@ from PySide6.QtGui import QColor
 
 from .base import BaseDialog
 from PySide6.QtWidgets import QMessageBox, QFileDialog, QColorDialog
-from .enums import Icon, Button
+from .enums import Icons, Buttons
 from typing import Callable, Self
 from logging import info, warning, error, critical
 
 class MessageBox(BaseDialog):
-    def __init__(self, title: str, content: str, icon: Icon = Icon.NoIcon, buttons: Button = Button.Ok) -> None:
+    def __init__(self, title: str, content: str, icon: Icons = Icons.NoIcon, buttons: Buttons = Buttons.Ok) -> None:
         super().__init__(title, content)
-        self._win: QMessageBox = QMessageBox()
+        self._win: QMessageBox = QMessageBox()  # pyright: ignore[reportIncompatibleVariableOverride]
         self._win.setWindowTitle(self.title)
         self._win.setText(self.content)
         self._win.setIcon(icon)
@@ -38,14 +38,14 @@ class MessageBox(BaseDialog):
         """
         event(self._win.question(None, self.title, self.get_content)) # Ask & call
 
-    def set_buttons(self, buttons: Button) -> Self:
+    def set_buttons(self, buttons: Buttons) -> Self:
         """Set the buttons of the message box."""
         self._win.setStandardButtons(buttons)
         return self
 class FileChooser(BaseDialog):
     def __init__(self, title: str) -> None:
         super().__init__(title, "")
-        self._win: QFileDialog = QFileDialog()
+        self._win: QFileDialog = QFileDialog()    # pyright: ignore[reportIncompatibleVariableOverride]
         self._win.setWindowTitle(self.title)
     def get_selections(self) -> list[str]:
         """Open the file dialog and return the selected file paths."""
@@ -58,7 +58,7 @@ class FileChooser(BaseDialog):
 class ColorPicker(BaseDialog):
     def __init__(self, title: str, content: str) -> None:
         super().__init__(title, content)
-        self._win: QColorDialog = QColorDialog()
+        self._win: QColorDialog = QColorDialog()  # pyright: ignore[reportIncompatibleVariableOverride]
         self._win.setWindowTitle(self.title)
     def get_color(self) -> str:
         """Open the color dialog and return the selected color in hex format."""
